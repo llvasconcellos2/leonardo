2128280cbbfb69dcc9d82a93296b40a70ad157ed
Leonardo Lima de Vasconcellos <leonardolimadevasconcellos@gmail.com>
2026-07-12 10:43:10 -0300

    renamed commands cheat sheet

--- submodule-commands.sh
+++ submodule-commands.sh
@@ -1,2 +1,3 @@
 git submodule add https://github.com/llvasconcellos2/devhouse-br.github.com.git .\project_archive\devhouse-br.github.com                                                                            
                                             
+git submodule update --init --recursive --remote
